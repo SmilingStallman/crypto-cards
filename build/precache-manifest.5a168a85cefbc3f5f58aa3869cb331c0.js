2d8b43381e72cbd204f405dad655516d228b7c6b
miskellk <kyle.miskell@cdk.com>
2019-11-14 15:51:18 -0600

    first prod build

---
+++ build/precache-manifest.5a168a85cefbc3f5f58aa3869cb331c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c159a0b8a8e778f78a0c6fae68cea34",
+    "url": "/crypto-cards/index.html"
+  },
+  {
+    "revision": "102aafad7d783b61d566",
+    "url": "/crypto-cards/static/css/main.7db20752.chunk.css"
+  },
+  {
+    "revision": "87f87388696d25fd31c9",
+    "url": "/crypto-cards/static/js/2.32c380b4.chunk.js"
+  },
+  {
+    "revision": "102aafad7d783b61d566",
+    "url": "/crypto-cards/static/js/main.4b449eaf.chunk.js"
+  },
+  {
+    "revision": "3c0da16681a22b4851ea",
+    "url": "/crypto-cards/static/js/runtime-main.94edb3af.js"
+  }
+]);
--- src/components/trader-list/trader-list.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import './trader-list.css'
-
-export const TraderList = props => {
-  return <div className='trader-list'>{ props.children }</div>;
-};
